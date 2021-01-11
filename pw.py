3a8d74ae87b1f96c7476254a64d4ddd898bf069b
JKakaofanatiker <53398711+JKakaofanatiker@users.noreply.github.com>
2021-01-11 17:00:07 +0100

    Add files via upload
    Version 1.1

--- pw.py
+++ pw.py
@@ -1,10 +1,14 @@
 import secrets
 import string
 from colorama import Fore # needed for color
+import pyperclip
 
 print(Fore.GREEN + "Enter password length:") # tell the user to type the length of the password
 length = input() # user input
 length = int(length)
 chars = string.digits + string.ascii_letters + string.punctuation # possible chars
 print(Fore.RED + "Here is your password:")
-print(''.join(secrets.choice(chars) for _ in range(length))) # prints password
+# print(''.join(secrets.choice(chars) for _ in range(length))) # prints password
+password = ''.join(secrets.choice(chars) for _ in range(length))
+print(password)
+pyperclip.copy(password)
